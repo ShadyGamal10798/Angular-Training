0ba00486f293f0242155bd6f6c2a100d1819af97
ShadyGamal <shade_EN30894828@f-eng.tanta.edu.eg>
2023-04-29 02:49:44 +0300

    first commint

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+// import Swiper core and required modules
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass'],
+  encapsulation:ViewEncapsulation.None
+})
+export class AppComponent {
+  title = 'swiper';
+
+
+}
